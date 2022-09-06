a93da1f92f0c9ca06ddc37cde6029cd49805ef89
oriolElias <Oriol.Elias@gft.com>
2022-09-06 08:13:28 +0000

    aa

--- moviedb/src/main/java/com/apirest/moviedb/service/MiddleManMovieDBService.java
+++ moviedb/src/main/java/com/apirest/moviedb/service/MiddleManMovieDBService.java
@@ -21,81 +21,56 @@ public class MiddleManMovieDBService {
     private StringBuilder json;
 
     public List<Genres> findAllGenreMovieList() throws IOException {
-
         StringBuilder jsonReq = getStringFromRequest("genre/movie/list?api_key=");
-
         Genres[] response  = mapper.readValue(mapper.readTree(jsonReq.toString()).get("genres").toString(),Genres[].class);
-
         return Arrays.asList(response);
-
     }
 
     public List<Movie> findPopularMovies() throws IOException {
-
         StringBuilder jsonReq = getStringFromRequest("movie/popular?api_key=");
-
         Movie[] response  = mapper.readValue(mapper.readTree(jsonReq.toString()).get("results").toString(),Movie[].class);
-
         return Arrays.asList(response);
-
     }
 
     public List<Movie> findTopRatedMovies() throws IOException {
         StringBuilder jsonReq = getStringFromRequest("movie/top_rated?api_key=");
-
         Movie[] response  = mapper.readValue(mapper.readTree(jsonReq.toString()).get("results").toString(),Movie[].class);
-
         return Arrays.asList(response);
-
     }
 
     public Movie findMovieById(Integer id) throws IOException {
         System.out.println(id.toString());
         StringBuilder jsonReq = getStringFromRequest("movie/"+id.toString()+"?api_key=");
-
         mapper.configure(DeserializationFeature.ACCEPT_SINGLE_VALUE_AS_ARRAY,true);
         Movie response  = mapper.readValue(jsonReq.toString(),Movie.class);
-
         return response;
     }
 
     public List<CastAndCrew> findCastAndCrewMovieById(Integer movie_id) throws IOException {
         StringBuilder jsonReq = getStringFromRequest("movie/"+movie_id.toString()+"/credits?api_key=");
-
         CastAndCrew[] response  = mapper.readValue(mapper.readTree(jsonReq.toString()).get("cast").toString(),CastAndCrew[].class);
-
         return Arrays.asList(response);
-
     }
     public List<Image> findAllImagesForMovieById(Integer movie_id) throws IOException {
         StringBuilder jsonReq = getStringFromRequest("movie/"+movie_id.toString()+"/images?api_key=");
-
         System.out.println(mapper.readTree(jsonReq.toString()).get("backdrops").toString());
-
         Image[] response  = mapper.readValue(mapper.readTree(jsonReq.toString()).get("backdrops").toString(),Image[].class);
-
         return Arrays.asList(response);
     }
     public List<Keywords> findAllKeywordsForMovieById(Integer movie_id) throws IOException {
         StringBuilder jsonReq = getStringFromRequest("movie/"+movie_id.toString()+"/keywords?api_key=");
-
         Keywords[] response  = mapper.readValue(mapper.readTree(jsonReq.toString()).get("keywords").toString(),Keywords[].class);
-
         return Arrays.asList(response);
 
     }
     public List<Movie> findRecommendationsForMovieById(Integer movie_id) throws IOException {
         StringBuilder jsonReq = getStringFromRequest("movie/"+movie_id.toString()+"/recommendations?api_key=");
-
         Movie[] response  = mapper.readValue(mapper.readTree(jsonReq.toString()).get("results").toString(),Movie[].class);
-
         return Arrays.asList(response);
     }
     public List<Movie> findSimilarMovieById(Integer movie_id) throws IOException {
         StringBuilder jsonReq = getStringFromRequest("movie/"+movie_id.toString()+"/similar?api_key=");
-
         Movie[] response  = mapper.readValue(mapper.readTree(jsonReq.toString()).get("results").toString(),Movie[].class);
-
         return Arrays.asList(response);
     }
     private StringBuilder getStringFromRequest(String req) throws IOException {
@@ -112,7 +87,4 @@ public class MiddleManMovieDBService {
         }
         return json;
     }
-
-
-
 }
